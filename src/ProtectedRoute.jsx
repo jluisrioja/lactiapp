68327baa25bf4b7d552c5bdcf5fe91374e3a8b48
jluisrioja <jorge.luis.rioja.saldivar@gmail.com>
2025-06-20 21:32:13 -0500

    Create ProtectedRoute.jsx

---
+++ src/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+// ProtectedRoute.jsx
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { auth } from "./firebase";
+
+const ProtectedRoute = ({ children }) => {
+  const user = auth.currentUser;
+  return user ? children : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
