ca6289700b1e84a1033d85663487aee2763114a9
jluisrioja <jorge.luis.rioja.saldivar@gmail.com>
2025-06-22 14:08:00 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ esto es lo que soluciona el error
-});
+  base: './', // 👈 IMPORTANTE
+})
