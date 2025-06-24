83a5de268619aee895df124145bcb8dc0d4880bc
jluisrioja <jorge.luis.rioja.saldivar@gmail.com>
2025-06-24 13:11:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 IMPORTANTE
+  base: '/', // 👈 Esto asegura que las rutas en producción sean absolutas
 })
