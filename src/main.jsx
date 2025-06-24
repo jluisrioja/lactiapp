628a3b945e413e9b6f4667468ed59dd1107d5485
jluisrioja <jorge.luis.rioja.saldivar@gmail.com>
2025-06-24 13:07:58 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => console.log('✅ Service Worker registrado:', reg))
+      .catch(err => console.error('❌ Error al registrar Service Worker:', err));
+  });
+}
